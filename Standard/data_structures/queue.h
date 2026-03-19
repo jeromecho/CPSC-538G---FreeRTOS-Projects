deec8b599f0f05e32b37686774314d59767df2d6
Jerome Cho <jeromejjcho@gmail.com>
2026-03-19 16:16:39 -0700

    feat(cbs): add additional features to queues

--- Standard/data_structures/queue.h
+++ Standard/data_structures/queue.h
@@ -15,4 +15,15 @@ typedef struct {
   uint16_t count;
 } Queue_t;
 
+void q_init(Queue_t *q, uint8_t *storage, size_t element_size, uint16_t max_elements);
+
+bool q_empty(Queue_t *q);
+
+bool q_enqueue(Queue_t *q, const void *item);
+
+/**
+ * @brief dequeue item if q has an element and out_item is not NULL
+ */
+bool q_dequeue(Queue_t *q, void *out_item);
+
 #endif // QUEUE_H
