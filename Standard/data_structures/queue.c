4090c6efb851827f8ca763f2ebf7a061ac5b3a4b
Jerome Cho <jeromejjcho@gmail.com>
2026-04-13 21:36:10 -0700

    chore(cbs): delete comments

--- Standard/data_structures/queue.c
+++ Standard/data_structures/queue.c
@@ -16,23 +16,13 @@ void q_init(Queue_t *q, uint8_t *storage, size_t element_size, uint16_t max_elem
   q->count        = 0;
 }
 
-bool q_empty(Queue_t *q) {
-  /*
-  printf("q_empty: q->count: %d\n", q->count);
-  */
-  return q->count == 0;
-}
+bool q_empty(Queue_t *q) { return q->count == 0; }
 
 // NB: critical sections below may be optional as long we guarantee no concurrency-related
 //     issues with accessing the queue
 bool q_enqueue(Queue_t *q, const void *item) {
   bool success = false;
   taskENTER_CRITICAL();
-
-  /*
-  printf("q_enqueue: q->element_size %d\n", q->element_size);
-  printf("q_enqueue: sizeof() %d\n", q->element_size);
-  */
 
   if (q->count < q->max_elements) {
     // Calculate destination address and copy bytes
@@ -67,9 +57,6 @@ bool q_dequeue(Queue_t *q, void *out_item) {
 bool q_top(Queue_t *q, void *out_item) {
   bool success = false;
   taskENTER_CRITICAL();
-
-  // printf("q_top: q->element_size %d\n", q->element_size);
-
   if (q->count > 0) {
     // Calculate source address and copy bytes
     uint8_t *src = q->buffer + (q->head * q->element_size);
