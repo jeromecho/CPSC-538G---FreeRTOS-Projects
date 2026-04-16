35eec076e97730de3be045ed3d14065671e40aef
Jerome Cho <jeromejjcho@gmail.com>
2026-04-16 15:21:22 -0700

    chore(smp): configuration constant setups

--- Standard/ProjectConfig.h
+++ Standard/ProjectConfig.h
@@ -7,8 +7,8 @@
 #define TEST_SUITE_PARTITIONED_MP 4
 #define TEST_SUITE_GLOBAL_MP      5
 
-#define TEST_SUITE 3
-#define TEST_NR    18
+#define TEST_SUITE 5
+#define TEST_NR    1
 
 #define SHARED_STACK_SIZE         (configMINIMAL_STACK_SIZE)
 #define MAX_TRACE_RECORDS         1000
